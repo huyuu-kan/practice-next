ad8e6dcf88349dc95584c2a848d3c2122ccdb9da
huyuu-kan <huyuu.kan.dev@gmail.com>
2025-10-16 18:32:35 +0900

    top:add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <div className="flex items-center justify-center w-screen h-screen">
       <div>
           <h1 className="text-4xl font-bold">Hello Next!</h1>
+          <p>
+            This is the main page.
+          </p>
          <Link href="/about">Go to About</Link>
       </div>
     </div>
